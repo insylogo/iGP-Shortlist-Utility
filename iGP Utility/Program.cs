3330e706fd997a5357821ea9891ed3321b141424
Rob Gibson <gibsonr@gmail.com>
2017-11-10 15:26:57 -0800

    Add project files.

---
+++ iGP Utility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iGP_Utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
